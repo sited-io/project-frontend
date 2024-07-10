82e849b1e12100d53061dcaa6c47f6f063c58a67
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-07-10 09:27:10 +0200

    remove dublicate meta tags

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,33 +1,25 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server";
+import { LANGUAGES } from "./locales";
 
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html>
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta
-            lang="en"
             name="description"
             content="Empowering individuals and businesses to effortlessly create websites and online shops with automated design and comprehensive ecommerce tools."
-          />
-          <meta
-            lang="de"
-            name="description"
-            content="Ermöglicht es Einzelpersonen und Unternehmen, mühelos Websites und Online-Shops mit automatisiertem Design und umfassenden E-Commerce-Werkzeugen zu erstellen."
           />
           <link rel="icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600&family=Noto+Sans:wght@300;400;500;600"
           />
-          <title lang="en">
+          <title>
             sited.io | effortlessly create websites and online shops
-          </title>
-          <title lang="de">
-            sited.io | erstelle mühelos Websites und Online-Shops
           </title>
 
           {assets}
