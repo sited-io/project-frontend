3288e4897f366c0800326fc28f745d9a4cec5dee
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-07-10 09:28:59 +0200

    remove dublicate meta tags

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,11 +1,10 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server";
-import { LANGUAGES } from "./locales";
 
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html>
+      <html lang="en">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
