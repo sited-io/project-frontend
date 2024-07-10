9341122ad2f6d1375e0d4ce32f2b3dee78d36a41
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-07-10 08:00:26 +0200

    landing page: adjust spacing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,10 +15,10 @@ export default function Home() {
     <>
       <section class="container max-w-screen-lg flex flex-col justify-center p-6 mb-6 mx-auto">
         <div class="flex flex-col justify-center text-center rounded-sm">
-          <h1 class="text-5xl font-display uppercase leading-none">
+          <h1 class="my-8 text-5xl font-display uppercase leading-tight lg:my-16">
             <Trans key={TKEYS["landing-page"].heading} />
           </h1>
-          <h2 class="mt-6 mb-8 text-lg font-display font-bold">
+          <h2 class="mt-0 mb-16 text-lg font-display lg:mb-24">
             <Trans key={TKEYS["landing-page"].subheading} />
           </h2>
         </div>
@@ -38,40 +38,40 @@ export default function Home() {
         {/* <h3 class="mb-6 text-2xl font-display font-bold">
           <Trans key={TKEYS["landing-page"]["why-choose-us"].title} />
         </h3> */}
-        <div class="flex flex-col justify-center lg:flex-row">
-          <div class="my-4 p-4 flex-1 rounded border-gray-300 dark:border-gray-500 lg:mx-2">
+        <div class="flex flex-col justify-center md:flex-row">
+          <div class="my-4 p-4 flex-1 text-center md:mx-2">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["why-choose-us"].sections[1].title}
               />
             </h4>
-            <p class="p-1 text-base text-slate-500 dark:text-gray-400">
+            <p class="p-1 text-base text-slate-600 dark:text-gray-400">
               <Trans
                 key={TKEYS["landing-page"]["why-choose-us"].sections[1].content}
               />
             </p>
           </div>
 
-          <div class="my-4 p-4 flex-1 rounded border-gray-300 dark:border-gray-500 lg:mx-2">
+          <div class="my-4 p-4 flex-1 text-center md:mx-2">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["why-choose-us"].sections[2].title}
               />
             </h4>
-            <p class="p-1 text-base text-slate-500 dark:text-gray-400">
+            <p class="p-1 text-base text-slate-600 dark:text-gray-400">
               <Trans
                 key={TKEYS["landing-page"]["why-choose-us"].sections[2].content}
               />
             </p>
           </div>
 
-          <div class="my-4 p-4 flex-1 rounded border-gray-300 dark:border-gray-500 lg:mx-2">
+          <div class="my-4 p-4 flex-1 text-center md:mx-2">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["why-choose-us"].sections[3].title}
               />
             </h4>
-            <p class="p-1 text-base text-slate-500 dark:text-gray-400">
+            <p class="p-1 text-base text-slate-600 dark:text-gray-400">
               <Trans
                 key={TKEYS["landing-page"]["why-choose-us"].sections[3].content}
               />
@@ -86,9 +86,9 @@ export default function Home() {
           <Trans key={TKEYS["landing-page"]["key-features"].title} />
         </h3>
         <div class="flex flex-col lg:grid lg:grid-cols-2">
-          <hr class="h-px col-span-full" />
+          {/* <hr class="h-px col-span-full border-gray-300 dark:border-gray-500" /> */}
 
-          <div class="my-4 p-4 rounded border-gray-300 dark:border-gray-500 lg:col-span-full">
+          <div class="my-4 p-4 lg:col-span-full">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["key-features"].features[1].title}
@@ -157,9 +157,9 @@ export default function Home() {
 
           <div class="hidden lg:block"></div>
 
-          <hr class="h-px col-span-full" />
+          <hr class="h-px col-span-full border-gray-300 dark:border-gray-500" />
 
-          <div class="my-4 p-4 rounded border-gray-300 dark:border-gray-500 lg:col-span-full">
+          <div class="my-4 p-4 lg:col-span-full">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["key-features"].features[2].title}
@@ -228,9 +228,9 @@ export default function Home() {
 
           <div class="hidden lg:block"></div>
 
-          <hr class="h-px col-span-full" />
+          <hr class="h-px col-span-full border-gray-300 dark:border-gray-500" />
 
-          <div class="my-4 p-4 rounded border-gray-300 dark:border-gray-500 lg:col-span-full">
+          <div class="my-4 p-4 lg:col-span-full">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["key-features"].features[3].title}
@@ -299,9 +299,9 @@ export default function Home() {
 
           <div class="hidden lg:block"></div>
 
-          <hr class="h-px col-span-full" />
+          <hr class="h-px col-span-full border-gray-300 dark:border-gray-500" />
 
-          <div class="my-4 p-4 rounded border-gray-300 dark:border-gray-500 lg:col-span-full">
+          <div class="my-4 p-4 lg:col-span-full">
             <h4 class="mb-4 text-xl font-display font-semibold">
               <Trans
                 key={TKEYS["landing-page"]["key-features"].features[4].title}
@@ -355,15 +355,15 @@ export default function Home() {
 
       {/* Ready to Launch Your Website? */}
       <section class="container max-w-screen-lg flex flex-col justify-center p-6 mt-6 mx-auto">
-        <h3 class="mb-6 text-2xl text-center font-display font-bold">
+        <h3 class="mb-6 text-center text-2xl font-display">
           <Trans
             key={TKEYS["landing-page"]["ready-to-launch-your-website"].title}
           />
         </h3>
-        <div class="flex flex-col justify-center lg:grid lg:grid-cols-2">
-          <div class="grid grid-cols-subgrid col-span-2 my-8 px-2 py-4">
-            <div class="">
-              <h4 class="text-xl text-center lg:text-start">
+        <div class="flex flex-col justify-center md:grid md:grid-cols-2">
+          <div class="my-8 px-2 py-4 md:grid md:grid-cols-subgrid md:col-span-2">
+            <div class="mb-8 md:m-0">
+              <h4 class="text-xl font-display font-semibold text-center md:text-start">
                 <Trans
                   key={
                     TKEYS["landing-page"]["ready-to-launch-your-website"]
@@ -371,7 +371,7 @@ export default function Home() {
                   }
                 />
               </h4>
-              <p class="text-base text-slate-600 text-center lg:text-start dark:text-gray-300">
+              <p class="text-base text-slate-600 text-center md:text-start dark:text-gray-400">
                 <Trans
                   key={
                     TKEYS["landing-page"]["ready-to-launch-your-website"]
@@ -381,9 +381,9 @@ export default function Home() {
               </p>
             </div>
 
-            <div class="mt-2 flex justify-center items-end lg:mt-0">
+            <div class="flex justify-center items-center">
               <A
-                class="mx-auto px-4 py-2.5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="px-4 py-2.5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 href={REGISTRATION_URL}
               >
                 <Trans key={TKEYS["landing-page"]["register"]} />
@@ -391,9 +391,9 @@ export default function Home() {
             </div>
           </div>
 
-          <div class="grid grid-cols-subgrid col-span-2 my-8 px-2 py-4">
-            <div>
-              <h4 class="text-xl text-center lg:text-start">
+          <div class="mx-auto my-8 px-2 py-4 md:grid md:grid-cols-subgrid md:col-span-2">
+            <div class="mb-8 md:m-0">
+              <h4 class="text-xl font-display font-semibold text-center md:text-start">
                 <Trans
                   key={
                     TKEYS["landing-page"]["ready-to-launch-your-website"]
@@ -401,7 +401,7 @@ export default function Home() {
                   }
                 />
               </h4>
-              <p class="text-base text-slate-600 text-center lg:text-start dark:text-gray-300">
+              <p class="text-base text-slate-600 text-center md:text-start dark:text-gray-400">
                 <Trans
                   key={
                     TKEYS["landing-page"]["ready-to-launch-your-website"]
@@ -410,7 +410,7 @@ export default function Home() {
                 />
               </p>
             </div>
-            <div class="mt-2 flex justify-center items-end lg:mt-0">
+            <div class="flex justify-center items-center">
               <A
                 class="px-4 py-2.5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 href={DASHBOARD_URL}
@@ -421,9 +421,9 @@ export default function Home() {
             </div>
           </div>
 
-          <div class="grid grid-cols-subgrid col-span-2 mt-8 mb-32 px-2 py-4">
-            <div>
-              <h4 class="text-xl text-center lg:text-start">
+          <div class="mt-8 mb-32 px-2 py-4 md:grid md:grid-cols-subgrid md:col-span-2">
+            <div class="mb-8 md:m-0">
+              <h4 class="text-xl font-display font-semibold text-center md:text-start">
                 <Trans
                   key={
                     TKEYS["landing-page"]["ready-to-launch-your-website"]
@@ -431,7 +431,7 @@ export default function Home() {
                   }
                 />
               </h4>
-              <p class="text-base text-slate-600 text-center lg:text-start dark:text-gray-300">
+              <p class="text-base text-slate-600 text-center md:text-start dark:text-gray-400">
                 <Trans
                   key={
                     TKEYS["landing-page"]["ready-to-launch-your-website"]
@@ -440,7 +440,7 @@ export default function Home() {
                 />
               </p>
             </div>
-            <div class="mt-2 flex justify-center items-end lg:mt-0">
+            <div class="flex justify-center items-center">
               <a
                 class="px-4 py-2.5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 href={"mailto:" + SUPPORT_EMAIL_ADDRESS}
